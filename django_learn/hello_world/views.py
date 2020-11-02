e75bd13124c839cd96d18d85103b5298e9fbff40
Manash Mondal <emailatmanash@gmail.com>
2020-11-02 13:02:30 +0600

    POST Method implemeted

--- django_learn/hello_world/views.py
+++ django_learn/hello_world/views.py
@@ -10,9 +10,13 @@ def home(request):
     return render(request,'index.html',{'name':'manash'})
 
 def addition(request):
-    val1 = int(request.GET['num1'])
-    # val1 = request.GET['num1']
-    val2 = int(request.GET["num2"])
+    # GET
+    # val1 = int(request.GET['num1'])
+    # val2 = int(request.GET["num2"])
+    
+    # POST
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST["num2"]) 
     res = val1+val2
     # return render(request, 'result.html')
     return render(request, 'result.html',{"result":res})
