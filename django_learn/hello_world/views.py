681123ad0ae1adc1dfaee782b62104a2a7a2e110
Manash Mondal <emailatmanash@gmail.com>
2020-11-02 12:53:40 +0600

    Get Method implemeted

--- django_learn/hello_world/views.py
+++ django_learn/hello_world/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
     # return HttpResponse("<h1>Hello World</h1>")
     # return render(request,'index.html')
-    return render(request,'index.html',{'name':'manash mondal'})
+    return render(request,'index.html',{'name':'manash'})
+
+def addition(request):
+    val1 = int(request.GET['num1'])
+    # val1 = request.GET['num1']
+    val2 = int(request.GET["num2"])
+    res = val1+val2
+    # return render(request, 'result.html')
+    return render(request, 'result.html',{"result":res})
